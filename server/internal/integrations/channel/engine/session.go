0c30a5b75dfe1856bd545416c9f7ea93e127b4fa
杨成伟 <yangchengwei.1@bytedance.com>
2026-07-17 14:31:12 +0800

    pr5486
    Signed-off-by: 杨成伟 <yangchengwei.1@bytedance.com>

--- server/internal/integrations/channel/engine/session.go
+++ server/internal/integrations/channel/engine/session.go
@@ -362,7 +362,7 @@ func (s *ChatSession) AppendUserMessage(ctx context.Context, in AppendInput) (Ap
 	if err := tx.Commit(ctx); err != nil {
 		return AppendResult{}, fmt.Errorf("commit: %w", err)
 	}
-	return AppendResult{IssueCommand: cmd, DedupMarked: markedInTx}, nil
+	return AppendResult{MessageID: message.ID, IssueCommand: cmd, DedupMarked: markedInTx}, nil
 }
 
 func isUniqueViolation(err error) bool {
